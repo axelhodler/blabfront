bc6e5de8953db9ed0bff19c3fad18d828e4bbbae
Axel Hodler <derxor@gmail.com>
2016-04-17 23:51:38 +0200

    implement create transaction

--- src/ledger/controllers/ledger_detail_controller.js
+++ src/ledger/controllers/ledger_detail_controller.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('blab')
-  .controller('LedgerDetailController', function($scope, $routeParams) {
+  .controller('LedgerDetailController', function($scope, $routeParams, $http, $localForage) {
     $scope.ethereumAddress = $routeParams.id;
 
     $scope.createTransaction = function() {
-      console.log('sending to', $scope.ethereumAddress, $scope.amountToSend);
+      $localForage.getItem('jwt').then(function(token) {
+        $http.post('http://localhost:3000/transactions',
+          {
+            to: $scope.ethereumAddress,
+            amount: $scope.amountToSend
+          },
+          {
+            headers: {
+              "Authorization": token
+            }
+          }
+        );
+      });
     }
 });
