1125c7f365eddca93a36f158f5bdb7020dba01a7
Axel Hodler <derxor@gmail.com>
2016-04-17 13:30:55 +0200

    put together small login sample

--- src/login/controllers/login_controller.js
+++ src/login/controllers/login_controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('blab')
-  .controller('LoginController', function($scope) {
-    $scope.login = 'login';
-});
+  .controller('LoginController', function($scope, $http, $localForage, $location) {
+    $scope.login = function() {
+      $http.post('http://localhost:3000/auth', {
+        'email': $scope.username,
+        'password': $scope.password}
+      ).then(function(response) {
+        $localForage.setItem('jwt', response.data);
+        $location.path('/ledger');
+      });
+    }
+  });
