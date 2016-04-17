d8fd7e499f6c563b2b84a44f57da72eae473848b
Axel Hodler <derxor@gmail.com>
2016-04-17 14:07:56 +0200

    rm log statement

--- src/ledger/controllers/ledger_controller.js
+++ src/ledger/controllers/ledger_controller.js
@@ -4,7 +4,6 @@ angular.module('blab')
   .controller('BlabController', function($scope, $http, $localForage) {
     $scope.text = [];
     $localForage.getItem('jwt').then(function(token) {
-      console.log('got', token);
       $http.get('http://localhost:3000/ledgers', {
         headers: {
           "Authorization": token
