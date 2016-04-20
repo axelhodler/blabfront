a754568f10698700b0cc343b4a7f7e06d766d42c
Axel Hodler <derxor@gmail.com>
2016-04-20 23:53:22 +0200

    use controllerAs for detail controller

--- src/ledger/controllers/ledger_detail_controller.js
+++ src/ledger/controllers/ledger_detail_controller.js
@@ -3,15 +3,15 @@
 angular.module('blab')
   .controller('LedgerDetailController', LedgerDetailController);
 
-function LedgerDetailController($scope, $routeParams, $http, $location, TokenRepository, REST_API_URL) {
-  $scope.ethereumAddress = $routeParams.id;
+function LedgerDetailController($routeParams, $http, $location, TokenRepository, REST_API_URL) {
+  this.ethereumAddress = $routeParams.id;
 
-  $scope.createTransaction = function() {
+  this.createTransaction = function() {
     TokenRepository.fetchToken().then(function(token) {
       $http.post(REST_API_URL + '/transactions',
         {
-          to: $scope.ethereumAddress,
-          amount: $scope.amountToSend
+          to: this.ethereumAddress,
+          amount: this.amountToSend
         },
         {
           headers: {
