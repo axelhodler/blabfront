1760622be35c9df03ba3991b845fefc04667ba03
Axel Hodler <derxor@gmail.com>
2016-04-21 23:32:19 +0200

    extract http usage to service

--- src/login/controllers/login_controller.js
+++ src/login/controllers/login_controller.js
@@ -3,15 +3,12 @@
 angular.module('blab')
   .controller('LoginController', LoginController);
 
-function LoginController($http, TokenRepository, REST_API_URL, $location) {
+function LoginController($location, TokenRepository, LoginService) {
   this.login = function () {
-    $http.post(REST_API_URL + '/auth', {
-        'email': this.username,
-        'password': this.password
-      }
-    ).then(function (response) {
-      TokenRepository.store(response.data);
-      $location.path('/ledger');
-    });
+    LoginService.login(this.username, this.password)
+      .then(function (response) {
+        TokenRepository.store(response.data);
+        $location.path('/ledger');
+      });
   }
 }
---
+++ src/login/services/login_service.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  angular.module('blab')
+    .service('LoginService', LoginService);
+
+  function LoginService($http, REST_API_URL) {
+    this.login = function(mail, password) {
+      return $http.post(REST_API_URL + '/auth', {
+          'email': mail,
+          'password': password
+        }
+      )
+    }
+  };
+})();
