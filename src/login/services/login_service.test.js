7af1c7608305e3ab41b390e0c961f99b814fd2b7
Axel Hodler <derxor@gmail.com>
2016-04-30 21:28:31 +0200

    cover login service

--- src/login/services/login_service.test.js
+++ src/login/services/login_service.test.js
@@ -1,15 +1,24 @@
 'use strict';
 
 describe('LoginService', function() {
-  var subject;
+  var subject,
+    http;
 
   beforeEach(module('blab'));
 
-  beforeEach(inject(function(_LoginService_) {
+  beforeEach(inject(function(_LoginService_, _$httpBackend_, _REST_API_URL_) {
     subject = _LoginService_;
+    http = _$httpBackend_;
+    this.REST_API_URL = _REST_API_URL_;
   }));
 
-  it('can be injected', function() {
-    expect(subject).toBeDefined();
+  it('logs in via post request', function() {
+    subject.login('mail@domain.com', 'pw');
+
+    http.expectPOST(this.REST_API_URL + '/auth', {
+      'email': 'mail@domain.com',
+      'password': 'pw'
+    }).respond({});
+    http.flush();
   })
 });
