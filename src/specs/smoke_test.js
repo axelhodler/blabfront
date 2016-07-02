681c033183cba6a7dc3e20a43824046f39034b15
Axel Hodler <derxor@gmail.com>
2016-07-02 14:21:06 +0200

    use ids to identify login elements

--- src/specs/smoke_test.js
+++ src/specs/smoke_test.js
@@ -4,9 +4,10 @@ describe('Blabfront', function() {
   it('works at least somewhat ;)', function() {
     browser.get('http://0.0.0.0:8080/');
 
-    element(by.id("username")).sendKeys("mail0@test.com");
-    element(by.xpath("//div[@class='col-md-12']/div/div/form/input[2]")).sendKeys("pw0");
-    element(by.css("input.btn.btn-primary")).click();
+    element(by.id("login_username")).sendKeys("mail0@test.com");
+    element(by.id("login_password")).sendKeys("pw0");
+    element(by.id("login_form_submit")).click();
+
     expect(element(by.tagName('html')).getText()).toContain("10000");
   });
 });
