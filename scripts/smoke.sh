c3abc8961922fbc2378f7496c85649223935c43f
Axel Hodler <derxor@gmail.com>
2016-07-02 21:46:23 +0200

    npm run smoketest against distributable

--- scripts/smoke.sh
+++ scripts/smoke.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 npm run lint
-npm run json-server & \
-npm start & \
 npm test
+npm run create_distributables
+npm run json-server & \
+npm run start-dist & \
 npm run protractor
 
 # cleanup
 git checkout -- api.json
 npm run kill-json-server
 npm run kill-server
-
