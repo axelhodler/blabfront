14e18b769cfe96f44d7874ce517e82c95c84f74e
Axel Hodler <derxor@gmail.com>
2016-07-10 22:27:44 +0200

    use files from master

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,7 @@ git checkout gh-pages
 rm -rf *
 git add .
 git commit -m "remove old stuff to avoid merge conflicts"
-git merge master -m "merge master to prepare release"
+git merge -X theirs master -m "merge master to prepare release"
 npm install
 npm run create_distributables
 # NOTE (optional): we could replace the REST API url here
