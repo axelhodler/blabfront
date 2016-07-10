d3e49f1064f8a0b97df01ac48d3a304539a84b47
Axel Hodler <derxor@gmail.com>
2016-07-10 18:48:59 +0200

    add ui test for detailview

--- src/specs/page_objects/overview.js
+++ src/specs/page_objects/overview.js
@@ -4,6 +4,10 @@ var Overview = function() {
   this.firstTokenAmountInList = function() {
     return element(by.id('overview_tokenamount_0')).getText();
   };
+
+  this.enterFirstUsersDetails = function() {
+    element(by.id('ledger_detailview_0')).click();
+  };
 };
 
 module.exports = Overview;
